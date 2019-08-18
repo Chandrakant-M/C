c0b5bec355461488ecc3b5d1d57c2c1d22a886fd
unknown <chan52drakant@gmail.com>
2019-08-18 12:34:36 +0530

    If statement: style 1 and 2

---
+++ Loops_and_conditionals.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+	int x=2;
+	int y=2;
+	// If statement style 1
+	if(x<y){
+		printf("x is less than y\n");
+	}
+	else if(x>y){
+		printf("x is greater than y\n");
+	}
+	else if(x==y){
+		printf("x is equal to y\n");
+	}
+	
+	// If statement style 2:
+	if( 1 ){
+		printf("This statement is always printed\n");
+	}
+	if( 0 ){
+		printf("This statement is never printed\n");
+	}
+	return 0;
+}
