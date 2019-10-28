d6091ec2117509262f62a07ff96e08b722a1a183
unknown <chan52drakant@gmail.com>
2019-10-28 17:11:35 +0530

    Qns 12 and 13 Chp 1 Exercises

--- OverviewOfC/ProgrammingExercises/Q12.c
+++ OverviewOfC/ProgrammingExercises/Q12.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
 int main(){
-	printf("The question is unclear.");
+	int a,b;
+	scanf("%d %d",&a,&b);
+	printf("%25s %10d %25s %10d\n","a=",a,"b=",b);
+	printf("%25s %10d %25s %10d\n","sum=",a+b,"difference=",a-b);
+	printf("%25s %10d %25s ","product=",a*b,"division=");
+	if(b==0)
+		printf("%10s","INFINITY");
+	else
+		printf("%10f",(float)a/b);
 	return 0;
 }
--- OverviewOfC/ProgrammingExercises/Q13.c
+++ OverviewOfC/ProgrammingExercises/Q13.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
+#include<Math.h>
 
 int main(){
-	printf("The question is unclear.");
+	int x1,y1,x2,y2;
+	float dist;
+	printf("Enter point 1 co-ordinates:");
+	scanf("%d %d",&x1,&y1);
+	printf("Enter point 2 co-ordinates:");
+	scanf("%d %d",&x2,&y2);
+	dist=sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2)));
+	printf("The distance between the two points is :%f",dist);
 	return 0;
 }
